4a3fd7434f88d47554c8ddd3918ab844a48b28cb
Filip Personal <filip_personal@Omnimaps-MacBook-Pro.local>
2019-10-12 14:54:01 +0200

    update plot data

--- PlotData.py
+++ PlotData.py
@@ -2,7 +2,10 @@ from collections import defaultdict
 
 import matplotlib.pyplot as plt
 import numpy as np
-
+def moving_average(a, n=3) :
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
 class AutoVivification(dict):
     """Implementation of perl's autovivification feature."""
     def __getitem__(self, item):
@@ -48,11 +51,11 @@ aggr_data = AutoVivification()
 for run_details, data in all_data.items():
     single_run_data = {k: [dic[k] for dic in data] for k in data[0]}
     for stat,list_of_lists in single_run_data.items():
-        try:
+        if stat == "losses":
+            continue
+        else:
             aggr_data[run_details][stat]["mean"] = np.mean(np.array(list_of_lists),axis=0)
             aggr_data[run_details][stat]["std_dev"] = np.std(np.array(list_of_lists),axis=0)
-        except:
-            continue
 
 # aggr_data has the form:
 # { args1: { "episodes : { "mean" : [...], "std_dev":[...] }, {"rewards" : ... }, } ...}
@@ -61,15 +64,18 @@ columns = 2
 # plot all data
 fig, axes = plt.subplots(rows, columns, figsize=(15, rows*4))
 for run_dets,metrics in aggr_data.items():
+    replay_type = run_dets.split("replay='")[1].split("'")[0]
     for i, (metric, stats) in enumerate(metrics.items()):
         mean = stats["mean"]
         std = stats["std_dev"]
-        upper = mean + std
-        lower = mean - std
+        # upper = mean + std
+        # lower = mean - std
         ax = axes[i // columns, i % columns]
-        ax.fill_between(range(len(mean)), lower, upper, alpha=0.5, color="gray")
-        ax.plot(upper, color="black", alpha=0.9, linestyle=":")
-        ax.plot(lower, color="black", alpha=0.9, linestyle=":")
+        # ax.fill_between(range(len(mean)), lower, upper, alpha=0.5, color="gray")
+        # ax.plot(upper, color="black", alpha=0.9, linestyle=":")
+        # ax.plot(lower, color="black", alpha=0.9, linestyle=":")
+        ax.plot(moving_average(mean,n= 10), alpha=0.9, label=replay_type)
+        ax.legend()
         ax.set_title(metric)
 
 plt.show()
